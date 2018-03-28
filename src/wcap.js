434780c7b279bedf6d2c239c248c50f00ba49d3f
David Stolarsky <david.stolarsky@nytimes.com>
2018-03-28 18:15:29 -0400

    bugfixes and feature improvements™

--- src/wcap.js
+++ src/wcap.js
@@ -1,34 +1,36 @@
 #!/usr/bin/env node
 
-var Nightmare = require('nightmare'),
-    vo = require('vo');
 const argv = require('yargs').argv;
 
-function * run() {
-    var nightmare = new Nightmare({
-        show: false,
-        width: 1024,
-        height: 768
-    });
-    var dimensions = yield nightmare.goto(argv._[0])
-        .wait('body')
-        .evaluate(function() {
-            var body = document.querySelector('body');
-            return { 
-                height: body.scrollHeight,
-                width:body.scrollWidth
-            }
-        });
+if(argv.timeout) {
+    setTimeout(() => {
+        console.error(`Timing out after --timeout=${argv.timeout}`);
+        process.exit(0);
+    }, +argv.timeout);
+}
 
-    console.dir(dimensions);
-    yield nightmare.viewport(dimensions.width, dimensions.height)
-        .wait(1000)
-        .screenshot(require('path')
-            .join(process.cwd(), argv.output));
+const Nightmare = require('nightmare');
+const path = require('path');
 
-    yield nightmare.end();
-}   
-
-vo(run)(function() {
-    console.log('done');
+var nightmare = new Nightmare({
+    show: false,
+    width: 1024,
+    height: 768
 });
+
+nightmare.goto(argv._[0])
+    .wait('body')
+    .wait(5000)
+    .evaluate(function() {
+        var body = document.querySelector('body');
+        return { 
+            height: body.scrollHeight,
+            width:body.scrollWidth
+        }
+    })
+    .then(dimensions => {
+        nightmare.viewport(argv.width ? +argv.width : dimensions.width, argv.height ? +argv.height : dimensions.height)
+            .wait(5000)
+            .screenshot(path.join(process.cwd(), argv.output))
+            .then(() => process.exit(0));
+    });
